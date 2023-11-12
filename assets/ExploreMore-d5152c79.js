4f9de87f5c49edcac8226ae53934e9ba3d9b57c1
“Sebastian <gabrielsebastian.romero@gmail.com>
2023-11-12 15:32:41 -0500

    fixed labels

---
+++ assets/ExploreMore-d5152c79.js
@@ -0,0 +1,1 @@
+import{j as o,aZ as r,aM as t}from"./index-1f15fb0e.js";function a(){return o.jsx(r,{to:"/home",children:o.jsx(t,{children:"Go back"})})}export{a as default};
--- assets/ExploreMore-df1d1e16.js
+++
@@ -1,1 +0,0 @@
-import{j as o,aZ as r,aM as t}from"./index-a5bc57fd.js";function a(){return o.jsx(r,{to:"/home",children:o.jsx(t,{children:"Go back"})})}export{a as default};
